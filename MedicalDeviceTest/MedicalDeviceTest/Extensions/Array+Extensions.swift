7e29e8fa90a1bd37df0c11b1a1d669903a3be0cf
Mikita <gentled.druzhinnik@gmail.com>
2022-04-26 17:53:37 +0300

    feat: add split array function

---
+++ MedicalDeviceTest/MedicalDeviceTest/Extensions/Array+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Array+Extensions.swift
+//  MedicalDeviceTest
+//
+//  Created by Mikita Shalima on 26.04.22.
+//
+
+import Foundation
+
+extension Array {
+    
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
